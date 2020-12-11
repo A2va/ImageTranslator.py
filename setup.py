f646c14247f588e351fe0667bf1ee5e5caf5fd8c
A2va <49582555+A2va@users.noreply.github.com>
2020-12-11 16:43:39 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+def load_requirements(path):
+    requirements = []
+    with open(path) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line.startswith("git+") or line.startswith("https:"):
+                continue
+            elif line.startswith("-r "):
+                requirements += load_requirements(line[3:])
+            else:
+                requirements.append(line)
+    return requirements
+
+
+required_packages = load_requirements("./requirements.txt")
+
+
+
+setup(
+    name='MyPackageName',
+    version='1.0.0',
+    url='https://github.com/mypackage.git',
+    author='Author Name',
+    author_email='author@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
