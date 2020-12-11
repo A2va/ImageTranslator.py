ed49dbcf0d374c6e4050c16636615b811b1f6e3a
A2va <49582555+A2va@users.noreply.github.com>
2020-12-11 16:49:22 +0100

    Fil the metadata

--- setup.py
+++ setup.py
@@ -19,12 +19,11 @@ required_packages = load_requirements("./requirements.txt")
 
 
 setup(
-    name='MyPackageName',
-    version='1.0.0',
-    url='https://github.com/mypackage.git',
-    author='Author Name',
-    author_email='author@gmail.com',
-    description='Description of my package',
+    name='ImageTranslator',
+    version='0.1a',
+    url='https://github.com/A2va/ImageTranslator.git',
+    author='A2va',
+    description='An image translator packages',
     packages=find_packages(),    
-    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+    install_requires=required_packages
 )
