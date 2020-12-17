41e12303ba30f4d0815752def54abd4092422919
A2va <49582555+A2va@users.noreply.github.com>
2020-12-17 19:07:11 +0100

    Add argument to model downloader script

--- download_model.py
+++ download_model.py
@@ -1,6 +1,32 @@
 import image_translator.model.model_easyocr as easyocr
 import image_translator.model.model_tesseract as tesseract
+import getopt, sys
+
+short_options = "m:"
+long_options = ["mode="]
 
 if __name__ == "__main__":
-    easyocr.download_models()
-    tesseract.download_models()
+
+    args = sys.argv
+    args = args[1:]
+
+    try:
+        arguments, values = getopt.getopt(args, short_options, long_options)
+    except getopt.error as err:
+        print (str(err))
+        sys.exit(2)
+
+    for arg, value in arguments:
+        if arg in ("-m", "--mode"):
+            if value=='tesseract':
+                tesseract.download_models()
+            elif value=='easyocr':
+                easyocr.download_models()
+            elif value=='all':
+                print('Downlaod all models')
+            else:
+                print("Error: Wrong argument value")
+                easyocr.download_models()
+                tesseract.download_models()
+        else:
+            print ("Error: Wrong argument")
