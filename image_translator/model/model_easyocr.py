1d3b063eec6337b886ec146e70547e5b9c8c5056
A2va <49582555+A2va@users.noreply.github.com>
2020-12-18 17:30:46 +0100

    Import model url from easyocr

--- image_translator/model/model_easyocr.py
+++ image_translator/model/model_easyocr.py
@@ -13,28 +13,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# from easyocr.config import model_url, DETECTOR_FILENAME
-model_url = {
-    'detector': ('https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/craft_mlt_25k.zip', '2f8227d2def4037cdb3b34389dcf9ec1'),
-    'latin.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/latin.zip', 'fb91b9abf65aeeac95a172291b4a6176'),
-    'chinese.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/chinese.zip', 'dfba8e364cd98ed4fed7ad54d71e3965'),
-    'chinese_sim.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/chinese_sim.zip', '0e19a9d5902572e5237b04ee29bdb636'),
-    'japanese.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/japanese.zip', '6d891a4aad9cb7f492809515e4e9fd2e'),
-    'korean.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/korean.zip', '45b3300e0f04ce4d03dda9913b20c336'),
-    'thai.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/thai.zip', '40a06b563a2b3d7897e2d19df20dc709'),
-    'devanagari.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/devanagari.zip', 'db6b1f074fae3070f561675db908ac08'),
-    'cyrillic.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/cyrillic.zip', '5a046f7be2a4f7da6ed50740f487efa8'),
-    'arabic.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/arabic.zip', '993074555550e4e06a6077d55ff0449a'),
-    'tamil.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/v1.1.7/tamil.zip', '4b93972fdacdcdabe6d57097025d4dc2'),
-    'bengali.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/v1.1.8/bengali.zip', 'cea9e897e2c0576b62cbb1554997ce1c'),
-    'telugu.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/v1.2/telugu.zip', 'f7576012a3abe593950c47bfa1bd8ddc'),
-    'kannada.pth': ('https://github.com/JaidedAI/EasyOCR/releases/download/v1.21/kannada.zip', 'c240c97e4adb8773b53b3b3dec728627'),
-}
-
-DETECTOR_FILENAME = 'craft_mlt_25k.pth'
-
+from easyocr.config import model_url, DETECTOR_FILENAME
 from image_translator.model.download import download_and_unzip
 import os
+
 def download_models():
 
     if not os.path.exists('./easyocr'):
@@ -48,4 +30,7 @@ def download_models():
     for name in model_url:
         if not os.path.exists(f'easyocr/model/{name}') and name!='detector':
             download_and_unzip(model_url[name][0],name,'easyocr/model')
+
+if __name__ == "__main__":
+    download_models()
     
