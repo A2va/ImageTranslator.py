bc197f742326b31fca1190843d7e9bbe98a26c56
A2va <49582555+A2va@users.noreply.github.com>
2021-06-18 18:44:56 +0200

    Add gen2 download

--- image_translator/download/model_easyocr.py
+++ image_translator/download/model_easyocr.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from easyocr.config import recognition_models, detection_models
-from image_translator.download.download import download_and_unzip
+from download import download_and_unzip
 import os
 
 
@@ -29,9 +29,21 @@ def download_models():
         download_and_unzip(
             detection_models['craft']['url'], detector_filename, 'easyocr/model', detector_filename)
 
-    print('EasyOCR models:\n')
+    print('EasyOCR models:')
+    print('Gen1')
     for model in recognition_models['gen1']:
         url = recognition_models['gen1'][model]['url']
         filename = recognition_models['gen1'][model]['filename']
         if not os.path.exists(f'easyocr/model/{filename}'):
             download_and_unzip(url, filename, 'easyocr/model', filename)
+
+    print('Gen2')
+    for model in recognition_models['gen2']:
+        url = recognition_models['gen2'][model]['url']
+        filename = recognition_models['gen2'][model]['filename']
+        if not os.path.exists(f'easyocr/model/{filename}'):
+            download_and_unzip(url, filename, 'easyocr/model', filename)
+
+
+if __name__ == '__main__':
+    download_models()
