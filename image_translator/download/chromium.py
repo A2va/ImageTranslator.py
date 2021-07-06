33bc7c6e2139410c99a821a354efee93339dc716
A2va <49582555+A2va@users.noreply.github.com>
2021-07-06 18:01:48 +0200

    Chromium downloader are only for windows

--- image_translator/download/chromium.py
+++ image_translator/download/chromium.py
@@ -1,9 +1,19 @@
 import pyppeteer.chromium_downloader as chromium
 from pathlib import Path
 import os
+import sys
+
+# Logging
+import logging
+log = logging.getLogger('image_translator')
 
 
 def download_chromium():
+
+    if sys.platform.startswith('linux'):
+        log.warn('Linux download are not supported. Install chrome or chromium.')
+        return
+
     if not os.path.exists('./chromium'):
         Path('./chromium').mkdir(parents=True)
     if not Path(f'./chromium/{chromium.REVISION}/{chromium.windowsArchive}/chrome.exe').exists():
