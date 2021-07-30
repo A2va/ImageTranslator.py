0fc2b622a685f5705e98b06696e04a4871365080
A2va <49582555+A2va@users.noreply.github.com>
2021-07-30 13:02:21 +0200

    Change Translator api

--- image_translator/translator.py
+++ image_translator/translator.py
@@ -20,7 +20,7 @@ log = logging.getLogger('image_translator')
 
 
 class Translator():
-    def __init__(self, translator: str, src_lang: str, dest_lang: str):
+    def __init__(self, translator: str, dest_lang: str, src_lang: str):
 
         # Check if ocr and language are available
         if translator not in list(TRANS.keys()):
@@ -45,45 +45,45 @@ class Translator():
             self.src_lang = TRANS_LANG[src_lang][TRANS[self.ocr]]
             self.dest_lang = TRANS_LANG[dest_lang][TRANS[self.ocr]]
 
-    def translate(self, paragraph: Paragraph) -> Paragraph:
+    def translate(self, text: str) -> str:
+        if self.translator == 'google':
+            return self._run_google(text, self.dest_lang, self.src_lang)
+        elif self.translator == 'bing':
+            return self._run_bing(text, self.dest_lang, self.src_lang)
+        elif self.translator == 'deepl':
+            return self._run_deepl(text, self.dest_lang, self.src_lang)
+
+    def translate_paragraph(self, paragraph: Paragraph) -> Paragraph:
         """
         Run translator between Google, Bing and DeepL
         """
-        log.debug('Run translator')
-        if self.translator == 'google':
-            return self.__run_google(paragraph, self.src_lang,
-                                     self.dest_lang)
-        elif self.translator == 'bing':
-            return self.__run_bing(paragraph, self.src_lang,
-                                   self.dest_lang)
-        elif self.translator == 'deepl':
-            return self.__run_deepl(paragraph, self.src_lang,
-                                    self.dest_lang)
+        paragraph['translated_text'] = self.translate(paragraph['text'])
+        return paragraph
 
-    def _run_google(self, paragraph: Paragraph, dest_lang: str, src_lang: str) -> str:
+    def _run_google(self, text: str, dest_lang: str, src_lang: str) -> str:
         """
         Run google translator
         """
         tra = Google()
 
-        paragraph['translated_text'] = tra.translate(paragraph['text'], dest_lang, src_lang).text
+        text = tra.translate(text, dest_lang, src_lang).text
 
-        return paragraph
+        return text
 
-    def _run_bing(self, paragraph: Paragraph, dest_lang: str, src_lang: str) -> str:
+    def _run_bing(self, text: str, dest_lang: str, src_lang: str) -> str:
         """
         Run bing translator
         """
         tra = Bing()
-        paragraph['translated_text'] = tra.translate(paragraph['text'], src_lang, dest_lang)
+        text = tra.translate(text, src_lang, dest_lang)
 
-        return paragraph
+        return text
 
-    def _run_deepl(self, paragraph: Paragraph, dest_lang: str, src_lang: str) -> str:
+    def _run_deepl(self, text: str, dest_lang: str, src_lang: str) -> str:
         """
         Run deepl translator
         """
         tra = DeepL(src_lang, dest_lang)
-        paragraph['translated_text'] = tra.translate(paragraph['text'])
+        text = tra.translate(text)
 
-        return paragraph
+        return text
