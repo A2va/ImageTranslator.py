e86ed6270398093e3341643f3eef706e2f3468c1
A2va <49582555+A2va@users.noreply.github.com>
2021-07-30 13:00:39 +0200

    Rename translator test and update to Translator class

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -1,9 +1,7 @@
 import unittest
 
-from googletrans import Translator as Google
-from image_translator.utils.deeplv2 import DeepL
+from image_translator.translator import Translator
 from image_translator.utils.bing import Bing
-
 
 class TestTranslator(unittest.TestCase):
     '''Testing translator'''
@@ -14,13 +12,12 @@ class TestTranslator(unittest.TestCase):
         self.expected_output = ["Ceci est un test",
                                 "Il s'agit d'un test",
                                 "C'est un test"]
-        self.src_lang = 'en'
-        self.dest_lang = 'fr'
+        self.src_lang = 'eng'
+        self.dest_lang = 'fra'
 
     def test_deepl(self):
         '''Test deepl translator'''
-
-        translator = DeepL(self.src_lang, self.dest_lang)
+        translator = Translator('deepl', self.dest_lang, self.src_lang)
         output = translator.translate(self.input)
 
         self.assertIn(output, self.expected_output)
@@ -28,16 +25,16 @@ class TestTranslator(unittest.TestCase):
     def test_google(self):
         '''Test google translator'''
 
-        translator = Google()
-        output = translator.translate(self.input, self.dest_lang, self.src_lang)
+        translator = Translator('google', self.dest_lang, self.src_lang)
+        output = translator.translate(self.input)
 
-        self.assertIn(output.text, self.expected_output)
+        self.assertIn(output, self.expected_output)
 
     def test_bing(self):
         '''Test bing translator'''
 
-        translator = Bing()
-        output = translator.translate(self.input, self.src_lang, self.dest_lang)
+        translator = Translator('bing', self.dest_lang, self.src_lang)
+        output = translator.translate(self.input)
 
         self.assertIn(output, self.expected_output)
 
